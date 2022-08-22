f6cae5bdfaaf8581423555dfefab86c53ee935fd
INSPIRON <INSPIRON@HOME-PC>
2022-08-22 20:39:52 +0400

    import-export-excelfile

--- Core/ECommerceAPI.Domain/EmailService/EmailSender.cs
+++ Core/ECommerceAPI.Domain/EmailService/EmailSender.cs
@@ -14,30 +14,32 @@ namespace ECommerceAPI.Domain.EmailService
     {
         public async Task SendEmailAsync(string email, string file)
         {
+            int check = email.IndexOf("@");
+            if (email.Substring(check + 1).ToLower() == "code.edu.az")
+            {
+                //mail content
+                MailMessage msg = new MailMessage();
+                //Attachment
+                msg.Attachments.Add(new Attachment(file.ToString()));
+                msg.IsBodyHtml = true;
+                msg.From = new MailAddress("Khanim.pg@code.edu.az", "");
+                msg.Subject = "Download Export file";
+                msg.To.Add(email);
+                msg.IsBodyHtml = true;
+                msg.Body = "Your report in the appropriate category for the date range you requested";
 
-            //mail content
-            MailMessage msg = new MailMessage();
-            //Attachment
-            msg.Attachments.Add(new Attachment(file.ToString()));
-            msg.IsBodyHtml = true;
-            msg.From = new MailAddress("Khanim.pg@code.edu.az", "");
-            msg.Subject = "Download Export file";
-            msg.To.Add(email);
-            msg.IsBodyHtml = true;
-            msg.Body = "Your report in the appropriate category for the date range you requested";
-
-          
-            //Server details
-            SmtpClient client = new SmtpClient();
-            client.UseDefaultCredentials = false;
-            client.Host = "smtp.gmail.com";
-            client.Port = 587;
-            client.EnableSsl = true;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("Khanim.pg@code.edu.az", "Novoresume00");
-            client.EnableSsl = true;
-            client.Send(msg);
+                //Server details
+                SmtpClient client = new SmtpClient();
+                client.UseDefaultCredentials = false;
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("aktivemailunvanidaxiledin", "aktivpassworddaxiledin");
+                client.EnableSsl = true;
+                client.Send(msg);
+            }
         }
     }
 }
